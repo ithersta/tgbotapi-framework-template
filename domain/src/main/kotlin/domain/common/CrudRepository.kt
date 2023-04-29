b76e76e0eabef83ce9fce0954f1e1381310a4b41
Владислав Шиляев <schilyaev.vlad@gmail.com>
2023-04-29 20:34:55 +0300

    Return Entity from CrudRepository::add

--- domain/src/main/kotlin/domain/common/CrudRepository.kt
+++ domain/src/main/kotlin/domain/common/CrudRepository.kt
@@ -1,7 +1,7 @@
 package domain.common
 
 interface CrudRepository<Entity : Any, NewEntity : Any, Id : Any> {
-    suspend fun add(entity: NewEntity): Id
+    suspend fun add(entity: NewEntity): Entity
     suspend fun get(id: Id): Entity
     suspend fun getAll(): List<Entity>
     suspend fun update(entity: Entity)
